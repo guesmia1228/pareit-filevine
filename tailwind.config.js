340e2f53aab253159e8c53b4aa5ff3791fbb08ac
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-07-04 00:31:53 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    600: "#104b23",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
